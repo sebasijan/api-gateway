a517db927910a8ab1c6684604bd4d4f9e026c35c
sebasijan <sebastian.otto.kotze@gmail.com>
2022-06-13 21:19:23 +0100

    .

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 func get(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost*")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	path := "https://" + strings.TrimPrefix(r.URL.String(), "/")
 
